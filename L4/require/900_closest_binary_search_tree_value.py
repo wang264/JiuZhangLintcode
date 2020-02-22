390ebd9848f68861050c6d79da4dd79af9387bea
Andy <wang264@hotmail.com>
2020-02-22 18:20:34 -0500

    number 900

--- L4/require/900_closest_binary_search_tree_value.py
+++ L4/require/900_closest_binary_search_tree_value.py
@@ -27,6 +27,65 @@ def build_binary_tree_from_list(arr):
     return root
 
 
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+class Solution:
+    """
+    @param root: the given BST
+    @param target: the given target
+    @return: the value in the BST that is closest to the target
+    """
+    def closestValue(self, root, target):
+        # write your code here
+
+        val, dis = self.closest_value_helper(root, target)
+        return val
+    def closest_value_helper(self, root, target):
+        if root is None:
+            return None, None
+        root_distance = abs(root.val - target)
+
+        left_val, left_distance = self.closest_value_helper(root.left, target)
+        right_val, right_distance = self.closest_value_helper(root.right, target)
+
+        # not child
+        if left_val is None and right_val is None:
+            return root.val, root_distance
+        # only right child
+        elif left_val is None and right_val is not None:
+            if right_distance<root_distance:
+                return right_val, right_distance
+            else:
+                return root.val, root_distance
+        # only left child
+        elif left_val is not None and right_val is None:
+            if left_distance<root_distance:
+                return left_val, left_distance
+            else:
+                return root.val, root_distance
+        # have both left and right child
+        else:
+            if root_distance <= left_distance and root_distance<= right_distance:
+                return root.val, root_distance
+            elif left_distance <= root_distance and left_distance <= right_distance:
+                return left_val, left_distance
+            elif right_distance <=root_distance and right_distance <=left_distance:
+                return right_val, right_distance
+
+
+sol = Solution()
+
+root = build_binary_tree_from_list(arr=[5,4,9,2,None,8,10])
+
+sol.closestValue(root, 4.6)
+
+'''
 class Solution:
     """
     @param root: the given BST
@@ -79,4 +138,4 @@ class Solution:
             else:
                 return upper
 
-
+'''
