5fe2e05f1ea1f548015c378b052bd890ef121754
Andy <wang264@hotmail.com>
2021-05-21 07:36:32 -0400

    2021/05/21

--- Intro/L5/require/480_binary_tree_paths.py
+++ Intro/L5/require/480_binary_tree_paths.py
@@ -1,1 +1,38 @@
 # Algorithm L4 require
+
+class Solution:
+    """
+    @param root: the root of the binary tree
+    @return: all root-to-leaf paths
+    """
+
+    def binaryTreePaths(self, root):
+        # write your code here
+        if root is None:
+            return []
+
+        curr_path = [str(root.val)]
+        rslt = []
+
+        self.dfs_helper(root, curr_path, rslt)
+
+        return rslt
+
+    def dfs_helper(self, node, curr_path, rslt):
+        if node.left is None and node.right is None:
+            # curr_path.append(str(node.val))
+            rslt.append("->".join(curr_path))
+            # curr_path.pop()
+            return
+
+        if node.left:
+            curr_path.append(str(node.left.val))
+            self.dfs_helper(node.left, curr_path, rslt)
+            curr_path.pop()
+
+        if node.right:
+            curr_path.append(str(node.right.val))
+            self.dfs_helper(node.right, curr_path, rslt)
+            curr_path.pop()
+
+
--- Intro/L5/require/97_maximum_depth_of_binary_tree.py
+++ Intro/L5/require/97_maximum_depth_of_binary_tree.py
@@ -1,1 +1,15 @@
 # Algorithm L4 optional
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """
+
+    def maxDepth(self, root):
+        if root is None:
+            return 0
+
+        left_depth = self.maxDepth(root.left)
+        right_depth = self.maxDepth(root.right)
+
+        return max(left_depth, right_depth) + 1
